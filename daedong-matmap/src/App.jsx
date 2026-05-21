5700f2b6a4ee57bdad845363d8a50b7e341b5cc9
choyeon-Han <choyeon8317@naver.com>
2026-05-21 16:48:35 +0900

    [feat] seperate components
    - 컴포넌트 분리 후 작업 파일 할당
    - Kakao Map API 연동 및 반응 확인

---
+++ daedong-matmap/src/App.jsx
@@ -0,0 +1,12 @@
+import Map from "./components/Map/Map.jsx";
+
+function App() {
+  return (
+    <div>
+      <h3>대동맛지도</h3>
+      <Map />
+    </div>
+  );
+}
+
+export default App;
---
+++ daedong-matmap/src/components/Map/Map.jsx
@@ -0,0 +1,57 @@
+// 지도를 불러오고 렌더링하는 컴포넌트
+import { useEffect, useRef } from 'react';
+
+const Map = () => {
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+        const kakaoKey = import.meta.env.VITE_KAKAO_JS_KEY;
+        const src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoKey}&autoload=false`;
+
+        const loadKakaoMap = () => {
+            if (window.kakao && window.kakao.maps) {
+                window.kakao.maps.load(() => {
+                    const container = mapRef.current;
+                    const options = {
+                        center: new window.kakao.maps.LatLng(37.5665, 126.9780),
+                        level: 5,
+                    };
+
+                    new window.kakao.maps.Map(container, options);
+                });
+                return;
+            }
+
+            const script = document.createElement("script");
+            script.src = `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${kakaoKey}&autoload=false`;
+            script.async = true;
+            // document.head.appendChild(script);
+
+            script.onload = () => {
+                window.kakao.maps.load(() => {
+                    const container = mapRef.current;
+                    const options = {
+                        center: new window.kakao.maps.LatLng(37.5665, 126.9780),
+                        level: 5,
+                    };
+
+                    new window.kakao.maps.Map(container, options);
+                });
+            };
+        };
+
+        loadKakaoMap();
+    }, []);
+
+    return (
+        <div
+            ref={mapRef}
+            style={{
+                width: "100%",
+                height: "500px",
+            }}
+        />
+   );
+};
+
+export default Map;
